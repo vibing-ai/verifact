3291d4447b38945fc9810f3c01d97de9980c3f70
ltevlin <ltevlin@gmail.com>
2025-06-02 17:41:03 +0200

    refactored test_faithfulness: moved metrics to be tested to a separate function

--- src/tests/test_faithfulness.py
+++ src/tests/test_faithfulness.py
@@ -62,13 +62,5 @@ async def test_faithfulness_real_output(query, expect_success, min_score):
                 print(f"Test Result list name: {test_result.name}")
                 for metric_data in test_result.metrics_data:
                     validate_metric(metric_data, expect_success, min_score)
-                        # assert metric_data.score > min_score, "Score should be greater than specified in the test data"
-                        # assert metric_data.success is expect_success
-                        # assert metric_data.reason is not None
-                        # assert "Truths" in metric_data.verbose_logs
-                        # assert "Claims" in metric_data.verbose_logs
-                        # assert "Verdicts" in metric_data.verbose_logs
-                        # print(f"metric reason: {metric_data.reason}")
-                        # print(f"verbose logs: {metric_data.verbose_logs}")
             else:
                 print(f"Test Result {test_result.name} list is None or empty")
